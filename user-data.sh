97f3905a3946450fee9d36294202b2a30c2945d1
harun <harunbulutus@gmail.com>
2024-04-15 00:19:37 +0300

    firtst

--- user-data.sh
+++ user-data.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 hostnamectl set-hostname docker_instance
- dnf update -y
- dnf install git -y
- dnf install docker -y
- systemctl start docker
- systemctl enable docker
- usermod -a -G docker ec2-user
+dnf update -y
+dnf install git -y
+dnf install docker -y
+systemctl start docker
+systemctl enable docker
+usermod -a -G docker ec2-user
 # install docker-compose
 curl -SL https://github.com/docker/compose/releases/download/v2.26.0docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
