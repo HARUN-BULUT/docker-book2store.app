612275b0e933cd51cb0383a6403ebaa9da814717
harun <harunbulutus@gmail.com>
2024-04-15 01:05:25 +0300

    modify

--- user-data.sh
+++ user-data.sh
@@ -8,8 +8,8 @@ systemctl start docker
 systemctl enable docker
 usermod -a -G docker ec2-user
 # install docker-compose
-curl -SL https://github.com/docker/compose/releases/download/v2.26.0docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 cd /home/ec2-user && git clone https://github.com/HARUN-BULUT/docker-book2store.app.git
 cd docker-book2store.app
 docker-compose up
